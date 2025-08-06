553408c5f03ecdc47329aae051d7db59fefcaa39
Emanuele-Comand <emanuelecomand137@gmail.com>
2025-08-06 05:56:06 +0200

    Added particles js background

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+particlesJS.load("particles-js", "/particles.json", () => {
+  console.log("particles caricato");
+});
+
 import initPassword from "./password.js";
 import initTooltip from "./tooltip.js";
 import customization from "./customization.js";
